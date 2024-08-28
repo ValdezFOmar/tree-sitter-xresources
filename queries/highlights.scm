befb3e8498d17d5babe510e98a9255c13b8dc57d
Omar Valdez <omarantoniovaldezf2@gmail.com>
2024-08-27 21:45:04 -0700

    feat(queries): Add highlight queries and examples

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,1 +1,36 @@
-; NOTE: add highlight queries here
+(comment) @comment
+
+"#include" @keyword.import
+
+"#define" @keyword.directive.define
+
+(define_directive
+  (identifier) @constant.macro)
+
+(component) @variable.member
+
+((component) @type
+  (#match? @type "^[A-Z]"))
+
+(components
+  (component) @property .)
+
+[
+  (string)
+  (resource_value)
+] @string
+
+[
+  "*"
+  (any_component)
+] @character.special
+
+[
+  "."
+  ":"
+] @punctuation.delimiter
+
+[
+  "\\"
+  (escape_sequence)
+] @string.escape
